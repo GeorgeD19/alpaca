81bf4e8d6b9391cd4e574ce6af1de8635ff33425
George Dunlop <george.d19@hotmail.co.uk>
2018-08-14 16:48:55 +0100

    Attachments
    This is useful if you want to attach external data against a field e.g. a camera image from cordova

--- src/js/Alpaca.js
+++ src/js/Alpaca.js
@@ -53,6 +53,7 @@
         var data = null;
         var schema = null;
         var options = null;
+        var attachments = [];
         var view = null;
         var callback = null;
         var renderedCallback = null;
@@ -209,6 +210,7 @@
             data = config.data;
             schema = config.schema;
             options = config.options;
+            attachments = config.attachments;
             view = config.view;
             callback = config.render;
             if (config.callback) {
